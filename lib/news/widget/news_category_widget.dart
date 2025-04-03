03c8dfdc86631ea39ff8097b5f36e70bee225501
Mahesh Sharma <mahesh.ddbank@gmail.com>
2025-04-04 01:17:28 +0545

    Layout: of news_cateogry under review

--- lib/news/widget/news_category_widget.dart
+++ lib/news/widget/news_category_widget.dart
@@ -12,7 +12,7 @@ class NewsCategoryWidget extends StatefulWidget {
 
 class _NewsCategoryWidgetState extends State<NewsCategoryWidget> {
   final List<String> categories = ['All news', 'Business', 'Sports', 'Tech', "Science"];
-  bool categoryClicked=false;
+  bool cat=false;
 
   @override
   Widget build(BuildContext context) {
@@ -22,38 +22,52 @@ class _NewsCategoryWidgetState extends State<NewsCategoryWidget> {
         padding: const EdgeInsets.all(16.0),
         child: 
         Row(
-          children: 
-            categories.map((category) => Padding(
-                    padding: const EdgeInsets.only(right: 10),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                        });
-                      },
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: category == categories.first
-                            ? Colors.white
-                            : Colors.black,
-                        backgroundColor: category == categories.first
-                            ? Color.fromARGB(255, 178, 18, 7)
-                            : Colors.grey[200],
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                      child: Text(category),
-                    ),
-                  ),
-                  )
-              .toList(),
- 
-          
-                         
-              
+          children: [
+            CatWidget(),
+          ],
+            // categories.map((category) => Padding(
+            //         padding: const EdgeInsets.only(right: 10),
+            //         child: ElevatedButton(
+            //           onPressed: () {
+            //             setState(() {
+            //             });
+            //           },
+            //           style: ElevatedButton.styleFrom(
+            //             foregroundColor: category == categories.first
+            //                 ? Colors.white
+            //                 : Colors.black,
+            //             backgroundColor: category == categories.first
+            //                 ? Color.fromARGB(255, 178, 18, 7)
+            //                 : Colors.grey[200],
+            //             shape: RoundedRectangleBorder(
+            //               borderRadius: BorderRadius.circular(20),
+            //             ),
+            //           ),
+            //           child: Text(category),
+            //         ),
+            //       ),
+            //       )
+            //   .toList(),
         ),
-
       ),
-      
     );
+  }
+}
+
+class CatWidget extends StatefulWidget {
+  const CatWidget({super.key});
+
+  @override
+  State<CatWidget> createState() => _CatWidgetState();
+}
+
+class _CatWidgetState extends State<CatWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: (){
+
+      },
+      child: Text('CLick Me'));
   }
 }
