13871fbfc17b6554636d33edcc5ebc543c7d2b80
Mahesh Sharma <mahesh.ddbank@gmail.com>
2025-04-07 04:47:31 +0545

    Flutter Layout

--- lib/flutter_layout/page/flutter_layout_page.dart
+++ lib/flutter_layout/page/flutter_layout_page.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/flutter_layout/widget/button_layout_widget.dart';
+import 'package:my_app/flutter_layout/widget/image_section_widget.dart';
 import 'package:my_app/flutter_layout/widget/title_layout_widget.dart';
+import 'package:my_app/flutter_layout/widget/text_section_widget.dart';
 
 class FlutterLayoutDemo extends StatelessWidget {
   const FlutterLayoutDemo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const String appTitle = 'F L U T T E R   L A Y O U T   D E M O';
+    const String appTitle = 'Some Beautiful Places';
     return 
       Scaffold(
-        appBar: AppBar(title: const Text(appTitle)),
+        appBar: AppBar(title: const Text(appTitle), centerTitle: true,),
         body: 
         const SingleChildScrollView(
           child: Column(
             children: [
-              TitleSection(
+                ImageSection(image: 'images/assets/lake.jpg'),
+                TitleSection(
                 name: 'Oeschinen Lake Campground',
-                location: 'Kandersteg, Switzerland'
+                location: 'Kandersteg, Switzerland',
                 ),
-              ButtonSection(),  
+                ButtonSection(),
+                TextSection(
+                  description:
+                      'Lake Oeschinen lies at the foot of the Blüemlisalp in the '
+                      'Bernese Alps. Situated 1,578 meters above sea level, it '
+                      'is one of the larger Alpine Lakes. A gondola ride from '
+                      'Kandersteg, followed by a half-hour walk through pastures '
+                      'and pine forest, leads you to the lake, which warms to 20 '
+                      'degrees Celsius in the summer. Activities enjoyed here '
+                      'include rowing, and riding the summer toboggan run.',
+                ),
               ],
             ),
         )
