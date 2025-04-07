8d25a227e2505dd96c9baac8e71d6cf3e5dd9408
Mahesh Sharma <mahesh.ddbank@gmail.com>
2025-04-07 06:15:59 +0545

    Removed unnecessary files in branch

--- lib/routes/route_page.dart
+++ lib/routes/route_page.dart
@@ -1,43 +1,13 @@
 import 'package:flutter/material.dart';
-
 import 'package:my_app/news/page/news_home_page.dart';
 import 'package:my_app/news/page/details_page.dart';
 
-import 'package:my_app/flutter_layout/page/flutter_layout_page.dart';
-import 'package:my_app/form/page/form_page.dart';
-import 'package:my_app/home/page/home_page.dart';
-
-// current directory for different pages of news
-import 'package:my_app/profile/page/profile_page.dart';
-import 'package:my_app/sport/page/extract_argument_screen.dart';
-import 'package:my_app/sport/page/sports_page.dart';
-import 'package:my_app/test/page/test_page.dart';
-import 'package:my_app/weather/page/weather_page.dart';
-import 'package:my_app/sport/page/sports_detail_page.dart';
-
 class AppRoute {
   static Map<String, WidgetBuilder> routes = {
-      "/":(context)=> HomePage(), 
-      "/home":(context)=> HomePage(),
+      "/":(context)=> NewsHomePage(), 
 
-      //current working directory
-      "/all_news_page":(context)=> NewsHomePage(),
+      "/news_home_page":(context)=> NewsHomePage(),
       "/news_details_page":(context)=> NewsDetailPage(),
-
-      "/weather":(context)=> WeatherPage(),
-
-      "/profile":(context)=> ProfilePage(),
-
-      "/form":(context)=>MyCustomForm(),
-
-      "/sports":(context)=> SportPage(),
-      "/sports_overview":(context)=> SportsDetailPage(),
-
-      "/test_page":(context)=>TestPage(),
-      "/flutterlayout":(context)=>FlutterLayoutDemo(),
-      "/extractArguments":(context)=>ExtractArgumentsScreen(),
-
-      "/test":(context)=>TestPage(),
     
     };
   }
