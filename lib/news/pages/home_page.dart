40d78d59c80d8402f7b669200b95e71cf2c596d4
Mahesh Sharma <mahesh.ddbank@gmail.com>
2025-04-08 21:24:29 +0545

    Initial commit

--- lib/news/pages/home_page.dart
+++ lib/news/pages/home_page.dart
@@ -1,35 +1,5 @@
 import 'package:flutter/material.dart';
-// import 'package:my_app/news/model/news_data_model.dart';
-import 'package:my_app/news/widget/news_appbar_widget.dart';
-import 'package:my_app/news/widget/news_category_widget.dart';
-import 'package:my_app/news/widget/news_featured_image.dart';
-import 'package:my_app/news/widget/news_latest_widget.dart';
-import 'package:my_app/news/widget/latest_news_tile.dart';
-import 'package:my_app/news/widget/news_bottom_navigation.dart';
-
-// const List<String> _categories = ['All news', 'Business', 'Sports', 'Tech', "Science"]; //list of categories in navigation bar
-const List<Map<String, dynamic>> _latestNewsData = [
-  {
-    'imageUrl': 'assets/images/lake.jpg',
-    'title': 'Awesome Trip Around the World',
-    'time': '4 hours ago',
-  },
-  {
-    'imageUrl': 'assets/images/lake.jpg',
-    'title': 'The Best Philosophy Locations',
-    'time': '1 day ago',
-  },
-  {
-    'imageUrl': 'assets/images/lake.jpg',
-    'title': 'Another Interesting Article',
-    'time': '2 days ago',
-  },
-  {
-    'imageUrl': 'assets/images/lake.jpg',
-    'title': 'Breaking Development in Technology',
-    'time': '3 days ago',
-  },
-];
+import 'package:my_app/news/pages/tabs.dart';
 
 class NewsHomePage extends StatefulWidget {
   const NewsHomePage({super.key});
@@ -38,57 +8,62 @@ class NewsHomePage extends StatefulWidget {
   State<NewsHomePage> createState() => _NewsHomePageState();
 }
 
-class _NewsHomePageState extends State<NewsHomePage> {
-  // int _selectedCategoryIndex = 0;
+class _NewsHomePageState extends State<NewsHomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  final List<Tab> categoryTabs = const [
+    Tab(text: 'General'),
+    Tab(text: 'Entertainment'),
+    Tab(text: 'Business'),
+    Tab(text: 'Health'),
+    Tab(text: 'Science'),
+    Tab(text: 'Sports'),
+    Tab(text: 'Technology'),
+  ];
+
+  @override
+  void initState() {
+    _tabController = TabController(length: categoryTabs.length, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            NewsAppBarWidget(),
-            NewsCategoryWidget(),
-            
-            // Container with fixed height for horizontal ListView
-            SizedBox(
-              height: 200, // Adjust height as needed
-              child: FeaturedNewsImage(
-              // imageUrl: "assets/images/lake.jpg",
-              imageUrl: 'https://cdn.pixabay.com/photo/2018/04/26/16/31/marine-3352341_1280.jpg',
-              title: "Beautiflul lake",
-              padding: EdgeInsets.all(16)),
-            
-            ),
-            
-            // Latest News heading
-            const Padding(
-              padding: EdgeInsets.only(left: 16.0, top: 16.0, bottom: 8.0),
-              child: LatestNewsHeading(headingText: "Latest News"),
-            ),
-            
-            // Latest News list - vertical scrolling
-            Expanded(
-              child: ListView.separated(
-                separatorBuilder: (context, index) => SizedBox(),
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                itemCount: _latestNewsData.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return NewsItemTile(
-                    imageUrl: _latestNewsData[index]['imageUrl']??'',
-                    title: _latestNewsData[index]['title']??'No Title',
-                    time: _latestNewsData[index]['time']??'',
-                  );
-                },
-              ),
-            ),
-            
-            // Bottom navigation
-            const NewsBottomNavigation(),
-          ],
+      appBar: AppBar(
+        actions: const [
+          Icon(
+            Icons.search,
+          ),
+          Icon(Icons.notifications)
+        ],
+        title: const Text('NewsApp'),
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(30.0),
+          child: TabBar(
+            controller: _tabController,
+            tabs: categoryTabs,
+            isScrollable: true,
+          ),
         ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          Articles(category: 'general'),
+          Articles(category: 'entertainment'),
+          Articles(category: 'business'),
+          Articles(category: 'health'),
+          Articles(category: 'science'),
+          Articles(category: 'sports'),
+          Articles(category: 'technology'),
+        ],
       ),
     );
   }
--- lib/routes/route_page.dart
+++ lib/routes/route_page.dart
@@ -3,7 +3,7 @@ import 'package:my_app/tourism/page/tourism_layout_page.dart';
 import 'package:my_app/form/page/form_page.dart';
 import 'package:my_app/home/page/home_page.dart';
 
-import 'package:my_app/news/pages/home_page.dart'; // current directory
+import 'package:my_app/news/pages/1home_page.dart'; // current directory
 
 import 'package:my_app/profile/page/profile_page.dart';
 import 'package:my_app/sport/page/extract_argument_screen.dart';
@@ -14,7 +14,7 @@ import 'package:my_app/sport/page/sports_detail_page.dart';
 
 class AppRoute {
   static Map<String, WidgetBuilder> routes = {
-      "/data":(context)=>TourismPage(), 
+      "/":(context)=>NewsHomePage(), 
       
       "/tourism":(context)=>TourismPage(), 
       //HomePage(), //current direcotry 
