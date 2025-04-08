cd0d1c76b1868418fe7eee16708dc17ea56d6443
Mahesh Sharma <mahesh.ddbank@gmail.com>
2025-04-08 19:17:54 +0545

    Initial commit

---
+++ lib/models/list_item_model.dart
@@ -0,0 +1,17 @@
+class ListItem {
+  String imgUrl;
+  String newsTitle;
+  String author;
+  String date;
+  String content;
+  String description;
+
+  ListItem(
+    this.imgUrl,
+    this.newsTitle,
+    this.author,
+    this.date,
+    this.content,
+    this.description,
+  );
+}
