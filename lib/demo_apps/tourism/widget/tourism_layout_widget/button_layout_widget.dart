1f8f00e75aa5d6a30fc122e29df78f8495f3c359
Mahesh Sharma <mahesh.ddbank@gmail.com>
2025-04-29 19:31:36 +0545

    Updated layout widget for tourism layout

--- lib/demo_apps/tourism/widget/tourism_layout_widget/button_layout_widget.dart
+++ lib/demo_apps/tourism/widget/tourism_layout_widget/button_layout_widget.dart
@@ -1,5 +1,14 @@
 
 import 'package:flutter/material.dart';
+// for adding phone call function
+import 'package:permission_handler/permission_handler.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+//for phone number
+final String phoneNumber="+1234567890";
+//for latitude and longitude
+final latitude = 37.4220;
+final longitude = -122.0841;
 
 class ButtonSection extends StatelessWidget {
   const ButtonSection({super.key});
@@ -12,14 +21,39 @@ class ButtonSection extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            ButtonWithText(color: color, icon: Icons.call, label: 'C A L L'),
-            ButtonWithText(color: color, icon: Icons.near_me, label: 'R O U T E'),
-            ButtonWithText(color: color, icon: Icons.share, label: 'S H A R E'),
+            InkWell(
+              onTap: _makePhoneCall, 
+              child: 
+                ButtonWithText(
+                  color: color, icon: Icons.call, label: 'Call Now', 
+                ),
+              ),
+            
+            InkWell(
+              onTap:_openMap,
+              child: 
+              ButtonWithText(
+                color: color, icon: Icons.near_me,
+                label: 'Find Route'
+                ),
+            ),
+            
+            InkWell(
+              onTap: _shareOnWhatsApp,
+              child:
+                ButtonWithText(
+                  color: color, 
+                  icon: Icons.share,
+                  label: 'Share to Friends'),
+
+            ),
           ],
         ),
       );
   }
-}
+
+  
+  }
 
 class ButtonWithText extends StatelessWidget {
   const ButtonWithText({
@@ -57,3 +91,56 @@ class ButtonWithText extends StatelessWidget {
       );
     }
 }
+
+// function for adding phone call function
+  Future<void> _makePhoneCall() async{
+  final status = await Permission.phone.status;
+    if(status.isGranted)
+    {
+      final Uri url = Uri.parse('tel:$phoneNumber');
+      if(await canLaunchUrl(url)){
+        await launchUrl(url);
+      }else{
+        throw 'Could not launch $phoneNumber';
+      }
+    }
+    else {
+      final result=await Permission.phone.request();
+      if(result.isGranted){
+        _makePhoneCall(); //Retry after permission granted
+      }else{
+        // permission denied
+        print('Phone call permission denied');
+      }
+    }
+}
+
+// function for adding phone call function
+  Future<void> _openMap() async{
+    //for address by using longitude and latitude
+
+  final googleMapsUrl = Uri.parse('https://www.google.com/maps/search/?api=1&query=$latitude,$longitude');
+
+  if(await canLaunchUrl(googleMapsUrl)){
+    await launchUrl(googleMapsUrl, mode:LaunchMode.externalApplication);
+  }else{
+    throw'Could not open the map for provided address';
+  }
+}
+
+final String message="Hi! Check out this aswesome place!";
+final String orgPhoneNumber="+9876543210"; 
+
+Future<void> _shareOnWhatsApp() async{
+  final encodeMessage=Uri.encodeComponent(message);
+
+  final url = Uri.parse("https://wa.me/?text=$encodeMessage"); // Without phone number
+    // final url = Uri.parse("https://wa.me/$phoneNumber?text=$encodedMessage"); // With phone number
+
+  if(await canLaunchUrl(url)){
+    await launchUrl(url, mode:LaunchMode.externalApplication);
+  }else{
+    throw 'Could not open WhatsApp';
+  }
+}
+
