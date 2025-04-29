7cf9d67b360b7e87964a1750153623b3ab33fb3f
Mahesh Sharma <mahesh.ddbank@gmail.com>
2025-04-29 13:08:47 +0545

    Modififed route page for easy handeling

--- lib/routes/route_page.dart
+++ lib/routes/route_page.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
 import 'package:my_app/demo_apps/home/page/home_page.dart';
+import 'package:my_app/demo_apps/profile/page/profile_page.dart';
+import 'package:my_app/demo_apps/tourism/page/tourism_layout_page.dart';
+import 'package:my_app/demo_apps/weather/page/weather_page.dart';
+import 'package:my_app/demo_apps/form/page/login_form_page.dart';
+// import 'package:my_app/news/screens/home_page.dart'
 
 class AppRoute {
   static Map<String, WidgetBuilder> routes = {
       "/":(context)=>kHomePage(), 
       "/dashboard":(context)=>kHomePage(),
+      "/tourism":(context)=>TourismPage(),
+      "/weather":(context)=>WeatherPage(),
+      "/login_form":(context)=>LoginFormPage(),
+      "/profile":(context)=>ProfilePage(),
+      // "/news_home":(context)=>kNewsPage(),
+      // "/sports_home":(context)=>kSportsPage(),
     };
   }
   
