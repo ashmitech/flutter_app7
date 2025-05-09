aef95b837008fdae8cdd2e8ab470527d93d35a98
Mahesh Sharma <mahesh.ddbank@gmail.com>
2025-05-09 15:32:40 +0545

    Added list to fetch city names

--- lib/demo_apps/weather/services/api_services.dart
+++ lib/demo_apps/weather/services/api_services.dart
@@ -13,3 +13,16 @@ Future<Map<String, dynamic>> fetchWeatherFromServer() async{
   final response = await Dio().get('http://localhost:3000/weather');
   return response.data;
 }
+
+Future<List<String>> fetchCityNames() async {
+  final response = await Dio().get('http://localhost:3000/weather'); // Use real endpoint
+
+  if (response.statusCode == 200) {
+    final data = response.data as List;
+
+    // Assuming each item is like { "location": "Kathmandu", "temperature": ..., "condition": ... }
+    return data.map<String>((item) => item['location'].toString()).toList();
+  } else {
+    throw Exception('Failed to fetch cities');
+  }
+}
