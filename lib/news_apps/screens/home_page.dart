6f441bc5ddc59f562cdb91edd447f70d9b19a533
Mahesh Sharma <mahesh.ddbank@gmail.com>
2025-05-16 16:40:37 +0545

    Added function to manage appbar navigation

--- lib/news_apps/screens/home_page.dart
+++ lib/news_apps/screens/home_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/widgets/dropdown_widget.dart' show MyDropdown;
+import 'package:my_app/demo_apps/profile/page/profile_page.dart';
 import 'package:my_app/news_apps/screens/main_home_page.dart';
 import 'package:my_app/news_apps/screens/search_page.dart';
 import 'package:my_app/news_apps/screens/bookmark_page.dart';
 import 'package:my_app/news_apps/screens/settings_page.dart';
+import 'package:my_app/news_apps/widgets/menu_button_widget.dart'; // appbar 
 
 import 'navigation/custom_bottom_nav.dart' show CustomBottomNav;
 
@@ -31,14 +32,30 @@ class _NewsHomePageState extends State<NewsHomePage> {
       SettingsPage(),
     ];
   }
-
   // Function to handle bottom navigation tap
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index; 
     });
   }
-
+  // function to handle the navigation menu
+  void _onMenuSelected(BuildContext context, String value){
+    switch (value){
+      case 'profile':
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (_)=> ProfilePage()),
+      );
+      case 'logout':
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Logged out")),
+        // TODO:
+        //to display message are you sure you want to logout when user press on logout menu
+        //if user press yes logout the screen with message on snack bar successfully logout.
+        // if user press no, continue to the same screen.
+      );
+      break;
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,13 +68,12 @@ class _NewsHomePageState extends State<NewsHomePage> {
         centerTitle: false,
         actions: [
           Padding(padding: EdgeInsets.fromLTRB(0, 0, 18.0, 0), 
-          child: 
-          InkWell(onTap: () => MyDropdown(),
-          child: Icon(Icons.menu, color: Colors.white70,),)
-          )
+          child: MenuButtonWidget(
+            onSelected: (value) => _onMenuSelected(context, value))
+          ),
         ],
       ),
-
+      // center build welcome to news application
       bottomNavigationBar: CustomBottomNav(
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
