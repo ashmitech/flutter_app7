0b8d832adc31f7dbdde28c99ab40746c6a311f29
Mahesh Sharma <mahesh.ddbank@gmail.com>
2025-06-02 08:32:12 +0545

    Added files from esewa documentation

--- esewa_flutter_sdk/lib/constants.dart
+++ esewa_flutter_sdk/lib/constants.dart
@@ -6,5 +6,5 @@ const String METHOD_CHANNEL_NAME = 'flutter_sdk_channel';
 const String PAYMENT_METHOD_SUCCESS = 'payment_success';
 const String PAYMENT_METHOD_FAILURE = 'payment_failure';
 const String PAYMENT_METHOD_CANCELLATION = 'payment_cancellation';
-const String CLIENT_ID = "JB0BBQ4aD0UqIThFJwAKBgAXEUkEGQUBBAwdOgABHD4DChwUAB0R";
-const String SECRET_KEY = "BhwIWQQADhIYSxILExMcAgFXFhcOBwAKBgAXEQ==";
+// const String CLIENT_ID = "JB0BBQ4aD0UqIThFJwAKBgAXEUkEGQUBBAwdOgABHD4DChwUAB0R";
+// const String SECRET_KEY = "BhwIWQQADhIYSxILExMcAgFXFhcOBwAKBgAXEQ==";
