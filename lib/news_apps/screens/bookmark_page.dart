6db7d01f712d66e3ce6236e2b55254aee394751e
Mahesh Sharma <mahesh.ddbank@gmail.com>
2025-06-04 16:41:54 +0545

    Added navigation to bookmark page from navigation page

--- lib/news_apps/screens/bookmark_page.dart
+++ lib/news_apps/screens/bookmark_page.dart
@@ -8,7 +8,30 @@ class BookmarkPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Bookmarked Items")),
+      appBar: 
+      AppBar(
+        // title: Text("Bookmarked Items")
+        backgroundColor: Colors.blue.shade600,
+        foregroundColor: Colors.white,
+        centerTitle:false,
+        title:Text("Bookmarks", 
+        style: TextStyle(
+          fontSize: 18.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.white
+        ),
+        ),
+        actions:
+         <Widget>[
+          IconButton(
+            icon: const Icon(Icons.bookmark),
+            tooltip: 'Edit Bookmark',
+            onPressed: () {
+              // handle the press
+            },
+          ),
+        ],
+        ),
       body:
           bookmarkedItems.isEmpty
               ? Center(child: Text("No Bookmarked Items"))
